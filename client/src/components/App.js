a027056fe77dbc4e2e40a7f0284aa4969b0c3908
Jonathan Grim <jonjongrim@gmail.com>
2017-07-31 15:57:01 -0400

    initial import

---
+++ client/src/components/App.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import '../css/App.css';
+import Customer from './Customer.js';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      product: []
+    };
+
+    this.handlePurchase = this.handlePurchase.bind(this);
+  }
+
+  componentDidMount() {
+    fetch('/customer')
+      .then(res => {
+        return res.json();
+      })
+      .then(product => {
+        this.setState({ product });
+      });
+  }
+
+  handlePurchase(event, product, quantity) {
+    event.preventDefault();
+    event.stopPropogation();
+    fetch('/customer', { method: 'POST', body: JSON.stringify({ product, quantity }) })
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ message: data.message });
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.message
+          ? <div>
+              <h1>
+                {this.state.message}
+              </h1>
+            </div>
+          : <div />}
+        <Customer product={this.state.product} fnPurchase={this.handlePurchase} />
+      </div>
+    );
+  }
+}
+
+export default App;
