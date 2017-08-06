837bdd728089d9cda244c89dfc1121864f4bb4e5
Jonathan Grim <jonjongrim@gmail.com>
2017-08-05 22:15:01 -0400

    Refactor out Store component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,58 +1,15 @@
 import React, { Component } from 'react';
 import '../css/App.css';
-import OrderForm from './OrderForm';
-import ProductTable from './ProductTable';
+import Store from './Store';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      products: []
-    };
-
-    this.handlePurchase = this.handlePurchase.bind(this);
-  }
-
-  componentDidMount() {
-    fetch('/customer')
-      .then(res => {
-        return res.json();
-      })
-      .then(products => {
-        this.setState({ products });
-      });
-  }
-
-  handlePurchase(event, product, quantity) {
-    event.preventDefault();
-    console.log(product, quantity);
-    fetch('/customer', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ product_id: product, quantity: quantity })
-    })
-      .then(res => res.json())
-      .then(data => {
-        this.setState({ message: data.message });
-      });
-  }
-
   render() {
     return (
       <div className="App">
-        {this.state.message
-          ? <div>
-              <h1>
-                {this.state.message}
-              </h1>
-            </div>
-          : <div />}
-        <ProductTable products={this.state.products} />
-        <OrderForm product={this.state.products} fnPurchase={this.handlePurchase} />
+        <Router>
+          <Route path="/shop" component={Store} />
+        </Router>
       </div>
     );
   }
