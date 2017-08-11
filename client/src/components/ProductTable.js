9c3d451302680edfd964155ffd59ea8a702ccb25
Jonathan Grim <jonjongrim@gmail.com>
2017-08-11 11:49:04 -0700

    Initial rework of product table

--- client/src/components/ProductTable.js
+++ client/src/components/ProductTable.js
@@ -1,36 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { formatPrice } from '../utils/utils';
 
 const ProductTable = props => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>Product ID</th>
-          <th>Product Name</th>
-          <th>Department Name</th>
-          <th>Price</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.products.map(product =>
-          <tr key={product.item_id}>
-            <td>
-              {product.item_id}
-            </td>
-            <td>
-              {product.product_name}
-            </td>
-            <td>
-              {product.department_name}
-            </td>
-            <td>
-              {product.price}
-            </td>
-          </tr>
-        )}
-      </tbody>
-    </table>
+    <div>
+      {props.products.map(product =>
+        <div key={product.item_id}>
+          <h3>
+            {product.product_name}
+          </h3>
+          <p>
+            {formatPrice(product.price)}
+          </p>
+        </div>
+      )}
+    </div>
   );
 };
 
