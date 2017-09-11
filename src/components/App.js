9abc0abe6f62319bcd547d940f6588d6b471abec
Jonathan Grim <jonjongrim@gmail.com>
2017-09-11 16:23:37 -0400

    Updates styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
-import '../css/App.css';
 import Nav from './Nav';
 import Store from './Store';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const appStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  fontFamily: "'Raleway', 'Quicksand', sans-serif",
+  height: '100%'
+};
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <div className="App">
+        <div style={appStyle}>
           <Nav />
           <Route exact path="/" render={() => <p>Hi</p>} />
           <Route path="/shop" component={Store} />
