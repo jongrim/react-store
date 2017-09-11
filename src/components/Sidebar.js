0d6724d8226778f135aea5a95565fa417b73ffa5
Jonathan Grim <jonjongrim@gmail.com>
2017-09-11 16:22:38 -0400

    restyle sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+const sidebarStyle = {
+  display: 'flex',
+  flexDirection: 'column',
+  flexBasis: '192px',
+  backgroundColor: '#f5f5f5'
+};
+
+const sidebarLinkStyle = {
+  textDecoration: 'none',
+  color: 'rgb(158, 158, 158)',
+  padding: '0.5em 0 0.25em 1em',
+  borderBottom: '1px solid #dddddd'
+};
+
+const activeLinkStyle = {
+  backgroundColor: 'rgba(25, 25, 25, 0.5)',
+  color: '#f5f5f5'
+};
 
 const Sidebar = props => {
-  return <div>{props.children}</div>;
+  return <div style={sidebarStyle}>{props.children}</div>;
 };
 
 const SidebarLink = props => {
   return (
-    <Link to={props.to}>
-      <p>{props.children}</p>
-    </Link>
+    <NavLink
+      style={sidebarLinkStyle}
+      activeStyle={activeLinkStyle}
+      to={props.to}>
+      {props.children}
+    </NavLink>
   );
 };
 
