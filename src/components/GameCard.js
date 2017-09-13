fc5013d296cc70efd9909a2fd65add5d60344184
Jonathan Grim <jonjongrim@gmail.com>
2017-09-13 17:20:07 -0400

    Add game data loading

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import ProductCard from './ProductCard.js';
+import API from '../utils/api.js';
 
 export default class GameCard extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      imgUrl: null
+    };
+  }
+
+  componentDidMount() {
+    API.getGame(this.props.id).then(result => {
+      this.setState({
+        imgUrl: result.imageUrl
+      });
+    });
   }
 
   render() {
-    return <ProductCard {...this.props} />;
+    return <ProductCard {...this.props} imgUrl={this.state.imgUrl} />;
   }
 }
